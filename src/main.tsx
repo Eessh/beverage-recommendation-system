0c28fc7244fa00091dad01e3b3a9756466f7c293
Eessh <eesshkumarjagana@gmail.com>
2022-06-11 11:19:01 +0530

    about state management

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,14 @@ import Recommendations from './components/Recommendations';
 import './index.css'
 import { GlobalContextProvider } from "./GlobalContext";
 
+/**
+ * React Router v6, passing props: https://ui.dev/react-router-pass-props-to-components
+ * Inorder to pass props from VideoComponent to AnalysisResults component:
+ *  1) AnalysisResults component should be child of VideoComponent
+ *  2) Use some kind of context
+ *  3) Move the state(age, gender, emotion) to App component, AnalysisResults component should be a child of App component
+ */
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
