29f4bd7d72ac5ad4944c264b2f751b4a7f92fc8b
Eessh <eesshkumarjagana@gmail.com>
2022-06-11 11:40:21 +0530

    updated GlobalContext

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -10,19 +10,43 @@ export interface IBeverage {
   quantityAvailable: number
 };
 
-interface IGlobalContext {
+type TEmotions = {
+  happy: number,
+  sad: number,
+  neutral: number,
+  angry: number,
+  surprised: number,
+  disgust: number
+};
+type TGlobalContext = {
+  age: number,
+  gender: string,
+  emotions: TEmotions,
   activeBeverageType: string,
   activeBeverage: IBeverage,
   moreInfoVisible: boolean,
+  setAge: React.Dispatch<React.SetStateAction<number>>,
+  setGender: React.Dispatch<React.SetStateAction<string>>,
+  setEmotions: React.Dispatch<React.SetStateAction<TEmotions>>,
   setActiveBeverageType: React.Dispatch<React.SetStateAction<string>>,
   setActiveBeverage: React.Dispatch<React.SetStateAction<IBeverage>>,
   setMoreInfoVisible: React.Dispatch<React.SetStateAction<boolean>>
 };
-interface IGlobalContextProviderProps {
+type TGlobalContextProviderProps = {
   children: React.ReactNode | React.ReactNode[]
 };
 
-const defaultContextValue: IGlobalContext = {
+const defaultContextValue: TGlobalContext = {
+  age: 24,
+  gender: "male",
+  emotions: {
+    happy: 1.00,
+    sad: 0.00,
+    neutral: 0.00,
+    angry: 0.00,
+    surprised: 0.00,
+    disgust: 0.00
+  },
   activeBeverageType: "Carbonated Drinks",
   activeBeverage: {
     title: "Coca Cola",
@@ -33,20 +57,29 @@ const defaultContextValue: IGlobalContext = {
     quantityAvailable: 21
   },
   moreInfoVisible: false,
+  setAge: () => {},
+  setGender: () => {},
+  setEmotions: () => {},
   setActiveBeverageType: () => {},
   setActiveBeverage: () => {},
   setMoreInfoVisible: () => {}
 }
 
-const GlobalContext = createContext<IGlobalContext>(defaultContextValue);
+const GlobalContext = createContext<TGlobalContext>(defaultContextValue);
 
-const GlobalContextProvider: React.FC<IGlobalContextProviderProps> = ({children}) => {
+const GlobalContextProvider: React.FC<TGlobalContextProviderProps> = ({children}) => {
+  const [age, setAge] = useState<number>(defaultContextValue.age);
+  const [gender, setGender] = useState<string>(defaultContextValue.gender);
+  const [emotions, setEmotions] = useState<TEmotions>(defaultContextValue.emotions);
   const [activeBeverageType, setActiveBeverageType] = useState<string>(defaultContextValue.activeBeverageType);
   const [activeBeverage, setActiveBeverage] = useState<IBeverage>(defaultContextValue.activeBeverage);
   const [moreInfoVisible, setMoreInfoVisible] = useState<boolean>(defaultContextValue.moreInfoVisible);
   
   return(
     <GlobalContext.Provider value={{
+      age, setAge,
+      gender, setGender,
+      emotions, setEmotions,
       activeBeverageType, setActiveBeverageType,
       activeBeverage, setActiveBeverage,
       moreInfoVisible, setMoreInfoVisible
@@ -56,7 +89,7 @@ const GlobalContextProvider: React.FC<IGlobalContextProviderProps> = ({children}
   );
 };
 
-const useGlobalContext = (): IGlobalContext => {
+const useGlobalContext = (): TGlobalContext => {
   const context = useContext(GlobalContext);
   if (context===null || context===undefined) {
     throw new Error("Error: useGlobalContext() can only be used inside of GlobalContextProvider.")
