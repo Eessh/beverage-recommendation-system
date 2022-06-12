94638085c70d408af82de1a63123517c8cd91e44
Kumar Rishabh <kr.rishabh72@gmail.com>
2022-06-12 19:13:03 +0530

    basic layout donw for analysis result screen

--- src/components/AnalysisResults/AnalysisResults.tsx
+++ src/components/AnalysisResults/AnalysisResults.tsx
@@ -1,40 +1,63 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext, TEmotions } from "../../GlobalContext";
 import { IBeveragePercent } from "../VideoComponent";
-import { AgeRanges, BeverageTypes, FemaleBeveragesData, MaleBeveragesData, EmotionsData } from "../VideoComponent/data";
+import { IconContext } from "react-icons";
+
+import {
+  AgeRanges,
+  BeverageTypes,
+  FemaleBeveragesData,
+  MaleBeveragesData,
+  EmotionsData,
+} from "../VideoComponent/data";
 import "./AnalysisResults.css";
+import { FaMale } from "react-icons/fa";
+import { FaFemale } from "react-icons/fa";
+import { FaChild } from "react-icons/fa";
+import { FaQuestion } from "react-icons/fa";
+import { GiFemale } from "react-icons/gi";
+import { GiMale } from "react-icons/gi";
+
+import { Gender } from "face-api.js";
+type ageIconProp = {
+  gender: string;
+  age: number;
+};
+
+type genderIconProp = {
+  gender: string;
+};
 
 const AnalysisResults = () => {
   const navigate = useNavigate();
-  const {
-    age,
-    gender,
-    emotions,
-    setRecommendations
-  } = useGlobalContext();
+  // const { age, gender, emotions, setRecommendations } = useGlobalContext();
+  const age = 14;
+  const gender = "male";
+  const emotions = "Happy";
 
   useEffect(() => {
     console.log("Log: Age: ", age);
     console.log("Log: Gender: ", gender);
     console.log("Log: Emotions: ", emotions);
-    setRecommendations({
-        ageGender: ageGenderRecommendations(gender, age),
-        emotions: emotionRecommendations(emotions),
-        weather: [],
-        temperature: [],
-        season: []
-    });
+    // setRecommendations({
+    //   ageGender: ageGenderRecommendations(gender, age),
+    //   emotions: emotionRecommendations(emotions),
+    //   weather: [],
+    //   temperature: [],
+    //   season: [],
+    // });
 
     // navigates to recommendations page in 5 seconds
-    setTimeout(() => {
-      navigate("/recommendations");
-    }, 5000);
+    // setTimeout(() => {
+    //   navigate("/recommendations");
+    // }, 5000);
   }, []);
 
   const getDominantEmotion = (emotions: TEmotions): string => {
-    let dominantEmotion: string = "", emotionValue: number = 0;
-    Object.entries(emotions).forEach(pair => {
+    let dominantEmotion: string = "",
+      emotionValue: number = 0;
+    Object.entries(emotions).forEach((pair) => {
       console.log("Log: Pair: ", pair);
       if (pair[1] > emotionValue) {
         dominantEmotion = pair[0];
@@ -98,11 +121,54 @@ const AnalysisResults = () => {
     }
   };
 
-  return(
-    <div className="AnalysisResults">
-      <span className="age">Estimated Age: {age}</span>
-      <span className="gender">Estimated Gender: {gender}</span>
-      <span className="emotion">Dominant Emotion: {getDominantEmotion(emotions)}</span>
+  const getAgeGroup = (age: number): string => {
+    if (age < 13) {
+      return "Child";
+    } else if (age < 19) {
+      return "Teenager";
+    } else if (age < 30) {
+      return "Young Adult";
+    } else if (age < 50) {
+      return "Adult";
+    } else {
+      return "Senior citizen";
+    }
+  };
+
+  const AgeIcon = (input: ageIconProp) => {
+    let gender = input.gender;
+    let age = input.age;
+    if (age < 15) {
+      return <FaChild size={80} />;
+    }
+
+    if (gender === "male") {
+      return <FaMale size={80} />;
+    } else if (gender === "female") {
+      return <FaFemale size={80} />;
+    } else return <FaQuestion size={80} />;
+  };
+
+  const GenderIcon = (input: genderIconProp) => {
+    let gender = input.gender;
+    if (gender === "male") return <GiMale size={80} />;
+    else if (gender === "female") return <GiFemale size={80} />;
+    else return <FaQuestion size={80} />;
+  };
+
+  return (
+    <div className="analysis-result-root">
+      <div className="age-container">
+        <AgeIcon gender={gender} age={age} />
+        <h1 className="age">{getAgeGroup(age)}</h1>
+      </div>
+      <div className="gender-container">
+        <GenderIcon gender={gender} />
+        <h1 className="gender">{gender}</h1>
+      </div>
+      {/* <span className="emotion">
+        Dominant Emotion: {getDominantEmotion(emotions)}
+      </span> */}
     </div>
   );
 };
