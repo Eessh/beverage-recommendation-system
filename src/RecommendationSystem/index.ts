c2dbf600f826e5b341dd06b4ee768d6f44b4dd84
Eessh <eesshkumarjagana@gmail.com>
2022-06-15 15:50:05 +0530

    added index file to RecommendationSystem

---
+++ src/RecommendationSystem/index.ts
@@ -0,0 +1,33 @@
+import {
+	ageGenderRecommendations,
+	emotionRecommendations,
+	weatherRecommendations,
+	temperatureRecommendations
+} from "./RecommendationSystem";
+import {
+	AgeGenderBeverageTags,
+	AgeRanges,
+	MaleBeveragesData,
+	FemaleBeveragesData
+} from "./AgeGenderData";
+import {
+	EmotionsData,
+	EmotionsWeights
+} from "./EmotionsData";
+import WeatherData from "./WeatherData";
+import TemperatureData from "./TemperatureData";
+
+export {
+	ageGenderRecommendations,
+	emotionRecommendations,
+	weatherRecommendations,
+	temperatureRecommendations,
+	AgeGenderBeverageTags,
+	AgeRanges,
+	MaleBeveragesData,
+	FemaleBeveragesData,
+	EmotionsData,
+	EmotionsWeights,
+	WeatherData,
+	TemperatureData
+};
