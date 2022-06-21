9d83f0e47513faf600ac8a7a4174b3c6bbaf5f47
Kumar Rishabh <kr.rishabh72@gmail.com>
2022-06-21 17:51:44 +0530

    settings button added without functionality

--- src/components/LaunchScreen/LaunchScreen.tsx
+++ src/components/LaunchScreen/LaunchScreen.tsx
@@ -4,11 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../GlobalContext";
 import { IBYLogo, BeverageLogo, DrinkLogo } from "../../assets/images";
 
+import { GiSettingsKnobs } from "react-icons/gi";
+import { AiFillSetting, AiOutlineSetting } from "react-icons/ai";
+
 const LaunchScreen = () => {
   const navigate = useNavigate();
   return (
     <div className="launch-screen-root">
-      <img src={IBYLogo} className="iby-logo-image" />
+      <div className="top-toolbar">
+        <img src={IBYLogo} className="iby-logo-image" />
+        <div className="settings-div">
+          <GiSettingsKnobs size={36} />
+        </div>
+      </div>
+
       <img src={BeverageLogo} className="drink-logo-image" />
       <h1>Get Drink that fits your mood!</h1>
       <div
@@ -17,7 +26,7 @@ const LaunchScreen = () => {
           navigate("/detectionScreen");
         }}
       >
-        click here
+        <h1>Get started</h1>
       </div>
     </div>
   );
