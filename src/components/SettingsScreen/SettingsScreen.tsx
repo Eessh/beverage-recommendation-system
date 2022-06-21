8e80bfbba6987652ed7919c96243aaa22211fdcd
Kumar Rishabh <kr.rishabh72@gmail.com>
2022-06-22 01:31:05 +0530

    minor changes

--- src/components/SettingsScreen/SettingsScreen.tsx
+++ src/components/SettingsScreen/SettingsScreen.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./SettingsScreen.css";
-
+import { AiOutlineClose } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 const SettingsScreen = () => {
-  return <div>Settings Screen</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="settings-screen-root">
+      <div className="settings-top-toolbar">
+        <h1>Settings</h1>
+
+        <AiOutlineClose
+          size={32}
+          className="settings-close-icon"
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsScreen;
