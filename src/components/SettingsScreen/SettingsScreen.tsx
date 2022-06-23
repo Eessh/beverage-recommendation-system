f4bb22cd9f38c3f8f8b5b51edd7f27dc9a3c6fbe
Kumar Rishabh <kr.rishabh72@gmail.com>
2022-06-23 17:02:10 +0530

    emotionsstate change tested and styling started

--- src/components/SettingsScreen/SettingsScreen.tsx
+++ src/components/SettingsScreen/SettingsScreen.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import "./SettingsScreen.css";
 import { AiOutlineClose } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../GlobalContext";
+import { TEmotionsData } from "../../Types";
 const SettingsScreen = () => {
   const navigate = useNavigate();
+  const { setEmotionsData } = useGlobalContext();
+
+  const testButtonHandler = () => {
+    const newEmotionsData: TEmotionsData = {
+      happy: ["Juices"],
+      sad: ["Chocolate", "Tea", "Coffee"],
+      neutral: ["Healthy", "Tea", "Coffee"],
+      angry: ["Tea", "Coffee"],
+      fear: ["Water"],
+      disgusted: ["Water"],
+      surprise: ["Water"],
+    };
+    setEmotionsData(newEmotionsData);
+  };
   return (
     <div className="settings-screen-root">
       <div className="settings-top-toolbar">
@@ -16,6 +32,21 @@ const SettingsScreen = () => {
             navigate("/");
           }}
         />
+      </div>
+      {/* <div className="retry-button" onClick={testButtonHandler}>
+        test button
+      </div> */}
+      <div className="settings-body">
+        <div className="emotions-tab-bar">
+          <div className="emotions-tab-button">Neutral</div>
+          <div className="emotions-tab-button">Happy</div>
+          <div className="emotions-tab-button">Sad</div>
+          <div className="emotions-tab-button">Surprise</div>
+
+          <div className="emotions-tab-button">Angry</div>
+          <div className="emotions-tab-button">Repulsive</div>
+          <div className="emotions-tab-button">Fear</div>
+        </div>
       </div>
     </div>
   );
