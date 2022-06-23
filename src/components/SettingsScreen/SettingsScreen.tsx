895ad81cb8f867b59c5c355e66474631aa999635
Kumar Rishabh <kr.rishabh72@gmail.com>
2022-06-23 19:49:17 +0530

    styling for settings done

--- src/components/SettingsScreen/SettingsScreen.tsx
+++ src/components/SettingsScreen/SettingsScreen.tsx
@@ -4,6 +4,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../GlobalContext";
 import { TEmotionsData } from "../../Types";
+import Switch from "react-switch";
 const SettingsScreen = () => {
   const navigate = useNavigate();
   const { setEmotionsData } = useGlobalContext();
@@ -48,11 +49,85 @@ const SettingsScreen = () => {
           <div className="emotions-tab-button">Fear</div>
         </div>
         <div className="emotions-option-container">
-          <div className="emotions-option-item">Healthy</div>
-          <div className="emotions-option-item">Coffee</div>
-          <div className="emotions-option-item">Chocolate</div>
-          <div className="emotions-option-item">Juice</div>
-          <div className="emotions-option-item">Tea</div>
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Healthy</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Coffee</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Chocolate</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Juice</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Tea</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
+
+          <div className="emotions-option-item">
+            <div className="emotion-option-itemName">
+              <h1>Water</h1>
+            </div>
+            <Switch
+              onChange={() => {}}
+              checked={true}
+              onColor="#ffd65c"
+              offColor="#555"
+              height={20}
+              width={48}
+            />
+          </div>
         </div>
       </div>
     </div>
